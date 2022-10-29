416ada5733d9f90aefeee23af5d8f5e49fa9e018
GastonRodrigo <cordoba.gastonr@yahoo.com>
2022-10-29 17:12:51 -0300

    segundo commit

--- WebFinal/models.py
+++ WebFinal/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from unittest.util import _MAX_LENGTH
 
 # Create your models here.
+class cliente(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    telefono = models.CharField(max_length=30)
+    direccion = models.CharField(max_length=60)
+
+class empleado(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellida = models.CharField(max_length=30)
+    area = models.CharField(max_length=30)
+    cargo = models.CharField(max_length=30)
+
+class proveedores(models.Model):
+    nombre = models.CharField(max_length=30)
+    tipo = models.CharField(max_length=30)
+    telefono = models.CharField(max_length=30)
+    direccion = models.CharField(max_length=60)
+
+class productos(models.Model):
+    modelo = models.CharField(max_length=30)
+    genero = models.CharField(max_length=30)
+    medidas = models.CharField(max_length=30)
