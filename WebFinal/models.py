f71afbd9d02763f6f2e82eb306bd86eb3cb7444f
gastonburela <gaston_burela@hotmail.com>
2022-11-01 08:13:28 -0300

    arreglado cargar static

--- WebFinal/models.py
+++ WebFinal/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from unittest.util import _MAX_LENGTH
 
-# Create your models here.
+# Create your models here.  
 class cliente(models.Model):
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
--- WebFinal/views.py
+++ WebFinal/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def inicio(request):
-    return render(request, '/Users/harkonen/Desktop/ProyectoFinal/ProyectoFinal/WebFinal/templates/inicio.html')
+    return render(request, 'I:/Python/Proyectos/ProyectoFinal/WebFinal/templates/inicio.html')
