0d0520cbe7da56341cd643482b23d47b7fa11c49
GastonRodrigo <cordoba.gastonr@yahoo.com>
2022-11-01 23:33:00 -0300

    agregados forms basicos

--- WebFinal/forms.py
+++ WebFinal/forms.py
@@ -7,5 +7,21 @@ class Formulario_cliente(forms.Form):
     telefono = forms.CharField(max_length=30)
     direccion = forms.CharField(max_length=60)
 
+class Formulario_empleado(forms.Form):
+    nombre = forms.CharField(max_length=30)
+    apellida = forms.CharField(max_length=30)
+    area = forms.CharField(max_length=30)
+    cargo = forms.CharField(max_length=30)
+
+class Formulario_proveedores(forms.Form):
+    nombre = forms.CharField(max_length=30)
+    tipo = forms.CharField(max_length=30)
+    telefono = forms.CharField(max_length=30)
+    direccion = forms.CharField(max_length=60)
+
+class Formulario_productos(forms.Form):
+    modelo = forms.CharField(max_length=30)
+    genero = forms.CharField(max_length=30)
+    medidas = forms.CharField(max_length=30)
 
 
